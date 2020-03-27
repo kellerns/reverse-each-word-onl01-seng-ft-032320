8fc6c08b2f3a1aa235d3e3bfece287782e2de826
Nicholas Keller <nic.keller328@gmail.com>
2020-03-27 17:41:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,10 @@
 def reverse_each_word(phrase)
   
   array = phrase.split(" ")
+  reversed_array = []
   array.each do |word|
-    word.split.reverse
+    reverse_word = word.split("").reverse.join("")
+    reversed_array << reverse_word
   end
+  reversed_array.join(" ")
 end
